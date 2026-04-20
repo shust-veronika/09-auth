63a60b05b00d35433a3b4e064938d461a3a9f339
veronika <sustv153@gmail.com>
2026-04-20 23:42:22 +0300

    dynamic

--- app/(private routes)/profile/page.tsx
+++ app/(private routes)/profile/page.tsx
@@ -3,6 +3,8 @@ import Image from "next/image";
 import Link from "next/link";
 import type { Metadata } from "next";
 
+export const dynamic = 'force-dynamic';
+
 export const metadata: Metadata = {
   title: "Profile",
   description: "User profile page",
