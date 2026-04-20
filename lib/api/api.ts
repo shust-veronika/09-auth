92b61a28f9dee755b6919c302a95649e1f2b2fa5
veronika <sustv153@gmail.com>
2026-04-20 18:36:12 +0300

    api

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,16 +1,24 @@
+import { CreateNoteDTO } from "@/types/note";
 import axios from "axios";
 
 const baseURL = process.env.NEXT_PUBLIC_API_URL + "/api";
-
-export const fetchNoteById = async (id: string) => {
-  return api.get(`/notes/${id}`);
-};
-
-export const createNote = async (data: CreateNoteDTO) => {
-  return api.post("/notes", data);
-};
 
 export const api = axios.create({
   baseURL,
   withCredentials: true,
 });
+
+export const fetchNotes = async () => {
+  const response = await api.get("/notes");
+  return response.data;
+};
+
+export const fetchNoteById = async (id: string) => {
+  const response = await api.get(`/notes/${id}`);
+  return response.data;
+};
+
+export const createNote = async (data: CreateNoteDTO) => {
+  const response = await api.post("/notes", data);
+  return response.data;
+};
